02a0ae775f90122175391deaaa4614544735b2ce
tortaepaisitphongse <tortaepaisitphongse@gmail.com>
2025-10-30 12:26:22 -0400

    additional testing for flow tool

---
+++ src/flow-tests/strict_null.js
@@ -0,0 +1,15 @@
+// // @flow
+
+// function square(n: number): number {
+//     return n * n;
+// }
+
+// // Intentional error: possibly null/undefined 
+// // passed where number is required
+// const maybeNum: ?number = null;
+// // $FlowExpectedError[incompatible-type-arg]
+// const badSquare = square(maybeNum);
+
+// const goodSquare = square(4);
+// console.log(goodSquare);
+
