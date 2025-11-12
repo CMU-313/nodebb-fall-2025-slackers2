f3ec47b35a2025b3a6f46625b440d1f4042e67d5
Raymond Welgosh <raymond.welgosh@gmail.com>
2025-11-12 00:53:55 +0000

    lint test fix - npm run test still failing

--- public/src/translate/index.js
+++ public/src/translate/index.js
@@ -1,12 +1,13 @@
 
-/* eslint-disable strict */
-var request = require('request');
-
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
- const TRANSLATOR_API = "http://host.docker.internal:5000"
- const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
- const data = await response.json();
- return ['is_english','translated_content'];
+	const TRANSLATOR_API = 'http://host.docker.internal:5000';
+	try {
+		const response = await fetch(`${TRANSLATOR_API}/?content=${encodeURIComponent(postData.content)}`);
+		const data = await response.json();
+		return [data.is_english || false, data.translated_content || postData.content];
+	} catch (err) {
+		return [false, postData.content];
+	}
 };
